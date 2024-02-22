fb23bf543a3cb8966b4c7d2e29e9ffac5d17b2e7
jvanderlei <joaop.vanderlei@gmail.com>
2024-02-21 21:55:06 -0300

    feat: added test for email validation

--- AuthenticationService.Tests/UserRegisterTests.cs
+++ AuthenticationService.Tests/UserRegisterTests.cs
@@ -4,15 +4,22 @@ namespace AuthenticationService.Tests;
 public class UserRegister
 {
     [Fact]
-    public void WhenSubmitValidUserInfo_ShouldAllowToRegister(){
-        string Name = "João Pedro Vanderlei";
-        string DateOfBirth = "02/01/2003";
-        string Address = "Av. Marechal Fiuza de Castro";
-        string CPF = "386.778.230-00";
-        string AnualSalary = "100.000,00";
-
+    public void UserSubmitName_ShouldReturnCorrectName()
+    {
         var user = new User();
+        user.Name = "João";
 
-        Assert.Equal(Name, user.Name);
+        Assert.Equal(user.Name, "João");
+    }
+
+    [Theory]
+    [InlineData("joao@gmail.com", true)]
+    [InlineData("xpto", false)]
+    public void WhenUserSubmitValidEmail_ShouldReturnIfItsValid(string email, bool isValid)
+    {
+        var user = new User();
+        bool emailIsValid = user.CheckValidEmail(email);
+
+        Assert.Equal(emailIsValid, isValid);
     }
 }
--- Domain/User.cs
+++ Domain/User.cs
@@ -1,19 +1,13 @@
-﻿namespace Domain;
+﻿using Microsoft.VisualBasic;
+
+namespace Domain;
 
 public class User
 {
-    public string Name {get; set;}
-    private string DateOfBirth {get; set;}
-    private string Address {get; set;}
-    private string CPF {get; set;}
-    private string AnualSalary {get; set;}
-
+    public string Name {get; set;}    
+    public string Email {get; set;}
     public User()
     {
-        this.Name = "João Pedro Vanderlei";
-        this.DateOfBirth = "02/01/2003";
-        this.Address = "Av. Marechal Fiuza de Castro";
-        this.CPF = "386.778.230-00";
-        this.AnualSalary = "100.000,00";
     }
+
 }
